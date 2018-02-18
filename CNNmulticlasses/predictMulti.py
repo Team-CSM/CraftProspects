89360ea5e4e0278b8af3453e76a543d1be21a45d
Craig Massie <2207400m@student.gla.ac.uk>
2018-02-18 13:57:51 +0000

    Fixing merge conflicts
    Former-commit-id: 71b111adb21d1df97353a48e5a33016553d827b1 [formerly 2acc2e2ed385ef6826b3393793de03e578016019]
    Former-commit-id: fc3afdc03020c44c187f316678c9059040b7e08b

--- CNNmulticlasses/predictMulti.py
+++ CNNmulticlasses/predictMulti.py
@@ -5,7 +5,7 @@ from keras.models import Sequential, load_model
 from PIL import Image
 import image_slicer
 
-img_width, img_height = 48, 48
+img_width, img_height = 64, 64
 model_path = 'model.h5'
 model_weights_path = 'weights.h5'
 model = load_model(model_path)
@@ -18,7 +18,7 @@ model.load_weights(model_weights_path)
 if not os.path.exists('./slices'):
     os.makedirs('./slices')
     print("slicing...")
-    tiles = image_slicer.slice('false_full_tile1.jpg', 2500, save=False)
+    tiles = image_slicer.slice('test.jpg', 2500, save=False)
     image_slicer.save_tiles(tiles, directory='slices/', prefix='slice')
     
 
@@ -35,8 +35,7 @@ for root, dirs, filenames in os.walk(location):
 
 print("imgstr: ", len(imgstr))
 
-class_to_name = ["slash_burn", "cloudy", "artisinal_mine"]
-
+class_to_name = ["clear_primary", "cloudy", "mine", "slash_burn"]
 
 for image in imgstr:
     x = load_img(image, target_size=(img_width,img_height))
@@ -45,24 +44,17 @@ for image in imgstr:
     x = np.expand_dims(x, axis=0)
     x = x/255
     
+    
     predictions = model.predict(x) #predictions in each class
     pred = np.argmax(predictions[0]) #winner index
 
-    out1 = class_to_name[pred]
+    if (max(predictions[0]) > 0.5):
+        out1 = class_to_name[pred]
 
-    #write coordinates to txt files to help out with input to game
-    f = open(out1+".txt", "a")
-    f.write(image[image.rfind('/')+7:-4].replace("_", ",") + "\n")
-    f.close() 
-
-
-    print("predictions: ", predictions)
-    print("img: ", image[image.rfind('/')+1:])
-
-    print("prediction = " + out1)
-
-
-    print("----------------------")
+        #write coordinates to log files to help out with input to game
+        f = open(out1+".log", "a")
+        f.write(image[image.rfind('/')+7:-4].replace("_", ",") + "\n")
+        f.close() 
 
 
 
