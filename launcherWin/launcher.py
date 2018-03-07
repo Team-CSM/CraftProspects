86feb10955e9739b3750a8c366dbebb6bfcb8fbc
Adrian <modoseinal@hotmail.com>
2018-03-07 20:29:04 +0000

    Changes to launcher, built game, added packaged launcher exe for windows
    Launcher and predictMulti combined
    Launcher packaged into working exe applcation for windows
    Game bugfixes and updates
    
    
    Former-commit-id: 0706c804aa69eb5a5ef6332bd7ce8a13549391f8 [formerly 5f56f583263cd61d2a7af879a4c452714a36f694]
    Former-commit-id: 6d3a380111a2aace53bd8464860cadcfefac3c86

--- launcherWin/launcher.py
+++ launcherWin/launcher.py
@@ -6,7 +6,14 @@ from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
 import os
 import subprocess
+from scipy import optimize
 import tkinter.font as font
+import sys
+import numpy
+from keras import *
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+import image_slicer
 
 #classes = burns, cloud, mines, clear
 
@@ -21,9 +28,24 @@ FONTBTN="Futura 12 bold"
 FONTLABEL="Futura 12 bold"
 CURSORBTN = "center_ptr"
 
+
+img_width, img_height = 64, 64
+model_path = 'model.h5'
+model_weights_path = 'weights.h5'
+model = load_model(model_path)
+model.load_weights(model_weights_path)
+
+class_to_name = ["clear", "cloudy", "mine", "slash"]
+
+# python predictMulti.py ./players_money/orig.jpg
+globalImgPath = "ohno"
+
+#print ("PREDICTMULTI imgPath: ", imgPath)
+#print ("PREDICTMULTI dirPath: ", dirPath)
+
 ######################################################################################
 def launchGame():
-    os.system("GameWin.exe") # for windows
+    os.system("WhackAMine.exe") # for windows
     # subprocess.call(["open", "GameMac3.app"]) #for mac
     #os.system("open GameMac.app")
     root.destroy()
@@ -58,7 +80,90 @@ def browse():
 
     uploadPathVar.set(source)
 
+def maine(imgPathe):
+    imgPath = imgPathe
+    dirPath = imgPath[:imgPath.rfind('\\')+1]
+    #dirPath = dirPath[1:]
+    #imgPath = imgPath[1:]
+    IMAGE = "."+imgPath
+    # location = dirPath + IMAGE.split('.')[0] + '_' + 'slices'
+    locationSlices = dirPath + 'slices'
+    locationSlices = locationSlices.replace("/", "\\")
 
+    print(imgPath)
+
+    if not os.path.exists(locationSlices):
+        os.makedirs(locationSlices)
+        print("make slices dir")
+        slice(900, IMAGE, locationSlices)
+
+    imgstr = []
+
+    # Add all slices to the imgstr
+    for root, dirs, filenames in os.walk(locationSlices):
+        for f in filenames:
+            if (f.startswith('.') == False):
+                imgstr.append(locationSlices + '/' + f)
+
+    print("imgstr: ", len(imgstr))
+
+
+
+    # Getting predictions for all the slices from the model
+    predict(imgstr, class_to_name, dirPath)
+
+    # outloc = dirPath
+    # if not os.path.exists(outloc):
+    # Finally splitting the input image into 9 slices, with set ratio to be input to game.
+    slice(9, IMAGE, dirPath)
+    
+    print("resizing...")
+    for root, dirs, filenames in os.walk(dirPath):
+        for f in filenames:
+            if f[-3:]==".png":
+                im = Image.open(dirPath + f)
+                nx, ny = im.size
+                im = im.resize((int(nx*1.4), ny), Image.BICUBIC)
+                im.save(dirPath + f)
+
+    # subprocess.call(["rm","-r",locationSlices]) #cleanup= delete the thousands of slices
+    os.system("rd /s /q " + locationSlices)
+    print("done")
+
+def slice(number, IMAGE, location):
+    # slice image for stream and add to the slices folder
+    
+    print("slicing...")
+    IMAGE = IMAGE[1:]
+    tiles = image_slicer.slice(IMAGE, number, save=False)
+    print("save in folder:", location)
+    image_slicer.save_tiles(tiles, directory=location, prefix='slice')
+    # image_slicer.save_tiles(tiles, directory=location)
+
+
+def predict(imgstr, class_to_name, dirPath):
+    print("predicting...")
+    for image in imgstr:
+        x = load_img(image, target_size=(img_width,img_height))
+
+        x = img_to_array(x)
+        x = numpy.expand_dims(x, axis=0)
+        # normalise
+        x = x/255
+        
+        predictions = model.predict(x) #predictions in each class
+        pred = numpy.argmax(predictions[0]) #winner index
+
+        if (max(predictions[0]) > 0.5):
+            out1 = class_to_name[pred]
+
+            #write coordinates to log files to help out with input to game
+            f = open(dirPath+out1+".txt", "a")
+            f.write(image[image.rfind('/')+7:-4].replace("_", ",") + "_")
+            f.close() 
+
+
+#maine()
 ######################################################################################
 def upload():
 
@@ -89,14 +194,20 @@ def upload():
     beforePathName = beforePathName.replace('/', "\\")
     afterPathName = newDir+"/orig.jpg"
     afterPathName = afterPathName.replace('/', "\\")
-
+    globalImgPath = afterPathName
     # subprocess.call(["move", beforePathName, afterPathName])
     os.system("move " + beforePathName + " " + afterPathName)
 
     # subprocess.call(["python", "predictMulti.py", afterPathName])
-    os.system("python predictMulti.py /" + afterPathName)
+    # os.system("python predictMulti.py /" + afterPathName)
+	
+    maine(afterPathName)
+	
+    #os.system("predictMulti.exe " + afterPathName)
 
     print("after predictiMulti.py ran")
+	
+    
     updateLsbox()
 
 ######################################################################################
@@ -291,3 +402,4 @@ root.mainloop()
 # lastPlayedImgBtn = Button(text="Select this last image you played", image=lastPlayedImg, compound="left", command=launchGame)
 # lastPlayedImgBtn.config(image=lastPlayedImg)
 # lastPlayedImgBtn.grid(row=3, columnspan=3, sticky="we")
+
