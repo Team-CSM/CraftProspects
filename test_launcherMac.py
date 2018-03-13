187e4ba735cf0e3376119faea16074376d79e0ef
Dan Cristian Cecoi <dancristian.cecoi@gmail.com>
2018-03-13 18:04:39 +0000

    created a unit test for launcherMac
    Former-commit-id: 05ec418a76ea9706fdd85b72f1cd6cffce18e507 [formerly 40dd41e38aa317709281ff268c4bdb6586d3dc41]
    Former-commit-id: e70324e8758da4f58a292cdf23ccc55325d956b2

---
+++ test_launcherMac.py
@@ -0,0 +1,79 @@
+import unittest 
+import os 
+import shutil
+from launcherMac.launcherMac import slice
+from launcherMac.launcherMac import predict
+from launcherMac.launcherMac import class_to_name
+from launcherMac.launcherMac import getImage
+from tkinter import * 
+
+class test_launcherMac(unittest.TestCase):
+    
+    # tests if number of slices created is equal to the number it was asked to be sliced.
+    def testSliceNumber(self):
+        if not os.path.exists("output/"):
+            os.makedirs("output/")
+        imagepath = "CI/orig.jpg"
+        number = 20
+        slice(number,imagepath,"output/")
+        list = os.listdir("output/")
+        number_files = len(list)
+        shutil.rmtree("output/")
+        self.assertEqual(number,number_files)
+    
+    def testPredict_text(self):
+        #check if the text files correspond to class_to_name.
+        #recreates the slices.
+        if not os.path.exists("output/"):
+            os.makedirs("output/")
+
+        imagepath = "CI/orig.jpg"
+        number = 20 
+        slice(number,imagepath,"output/")
+        list = os.listdir("output/")
+        
+        if not os.path.exists("output/text/"):
+            os.makedirs("output/text/")
+        
+        for var in range(len(list)):
+            list[var] = "output/"+list[var]
+        
+        predict(list,class_to_name,"output/text/")
+        list = os.listdir("output/text/")
+        
+        for file in list:
+            name = file[:-4]
+            if name in class_to_name:
+                boolean = True
+            else:
+                boolean = False
+        self.assertEqual(boolean,True)
+
+        #test the coordinates:
+        coordinates_list = []
+        for file in list:
+            file_object = open("output/text/"+file, "r")
+            text = file_object.read()
+            coordinates_list.extend(text.split(','))
+        
+        self.assertEqual(len(coordinates_list),number)
+        shutil.rmtree("output/text/")
+        shutil.rmtree("output/")
+
+    def testGetImage(self):
+        root = Tk()
+        imagepath = "CI/orig.jpg"
+        img = getImage(imagepath)
+        height = img.height()
+        width = img.width()
+        root.destroy()
+        self.assertEqual(height,165)
+        self.assertEqual(width,165)
+
+    
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
