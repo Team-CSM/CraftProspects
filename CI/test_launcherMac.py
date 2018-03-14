5b17ffca4ea26a4ce9b0cfec6079039b18733cd9
Craig Massie <2207400m@student.gla.ac.uk>
2018-03-14 19:02:53 +0000

    testing CI without h5 tests
    Former-commit-id: 44c7dd69636b594e8fc30a7932e40fb1848e14a4 [formerly 0052f36feaf7cfd0a8aa1dceac93ff41b8e1542f]
    Former-commit-id: 232fb521089523f823fc3fb1dd2a7817150fd30e

--- CI/test_launcherMac.py
+++ CI/test_launcherMac.py
@@ -25,75 +25,75 @@ class test_launcherMac(unittest.TestCase):
         shutil.rmtree("output/")
         self.assertEqual(number,number_files)
     
-    def testPredict_text(self):
+    # def testPredict_text(self):
         
-        # model = Sequential()
-        # model.add(Conv2D(32, (3, 3), input_shape=(150,150,3)))
-        # model.add(Activation('relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
+    #     # model = Sequential()
+    #     # model.add(Conv2D(32, (3, 3), input_shape=(150,150,3)))
+    #     # model.add(Activation('relu'))
+    #     # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        # model.add(Conv2D(32, (3, 3)))
-        # model.add(Activation('relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
+    #     # model.add(Conv2D(32, (3, 3)))
+    #     # model.add(Activation('relu'))
+    #     # model.add(MaxPooling2D(pool_size=(2, 2)))
 
-        # model.add(Conv2D(64, (3, 3)))
-        # model.add(Activation('relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-        # model.add(Flatten())
-        # model.add(Dense(64))
-        # model.add(Activation('relu'))
-        # model.add(Dropout(0.5))
-        # model.add(Dense(1))
-        # model.add(Activation('sigmoid'))
-        # model.save("test.h5")
-            # Loads the pretrained 19-layer network.
-        # base_model = VGG19(include_top=False,
-        #                 weights='imagenet',
-        #                 input_shape=(64, 64, 3))
-        # model = Sequential()
-        # model.add(base_model)        
-        # model.add(Flatten())
-        # model.add(Dense(4, activation='softmax'))
-        # model.save("test.h5")
-        # #check if the text files correspond to class_to_name.
-        # #recreates the slices.
-        if not os.path.exists("output/"):
-            os.makedirs("output/")
+    #     # model.add(Conv2D(64, (3, 3)))
+    #     # model.add(Activation('relu'))
+    #     # model.add(MaxPooling2D(pool_size=(2, 2)))
+    #     # model.add(Flatten())
+    #     # model.add(Dense(64))
+    #     # model.add(Activation('relu'))
+    #     # model.add(Dropout(0.5))
+    #     # model.add(Dense(1))
+    #     # model.add(Activation('sigmoid'))
+    #     # model.save("test.h5")
+    #         # Loads the pretrained 19-layer network.
+    #     # base_model = VGG19(include_top=False,
+    #     #                 weights='imagenet',
+    #     #                 input_shape=(64, 64, 3))
+    #     # model = Sequential()
+    #     # model.add(base_model)        
+    #     # model.add(Flatten())
+    #     # model.add(Dense(4, activation='softmax'))
+    #     # model.save("test.h5")
+    #     # #check if the text files correspond to class_to_name.
+    #     # #recreates the slices.
+    #     if not os.path.exists("output/"):
+    #         os.makedirs("output/")
 
-        imagepath = "CI/orig.jpg"
-        number = 20 
-        slice(number,imagepath,"output/")
-        list = os.listdir("output/")
+    #     imagepath = "CI/orig.jpg"
+    #     number = 20 
+    #     slice(number,imagepath,"output/")
+    #     list = os.listdir("output/")
         
-        if not os.path.exists("output/text/"):
-            os.makedirs("output/text/")
+    #     if not os.path.exists("output/text/"):
+    #         os.makedirs("output/text/")
         
-        for var in range(len(list)):
-            list[var] = "output/"+list[var]
+    #     for var in range(len(list)):
+    #         list[var] = "output/"+list[var]
         
-        predict(list,class_to_name,"output/text/",load_model("CI/model.h5"))
-        list = os.listdir("output/text/")
+    #     predict(list,class_to_name,"output/text/",load_model("CI/model.h5"))
+    #     list = os.listdir("output/text/")
         
-        boolean = False
-        for file in list:
+    #     boolean = False
+    #     for file in list:
            
-            name = file[:-4]
-            if name in class_to_name:
-                boolean = True
-            else:
-                boolean = False
-        self.assertEqual(boolean,True)
+    #         name = file[:-4]
+    #         if name in class_to_name:
+    #             boolean = True
+    #         else:
+    #             boolean = False
+    #     self.assertEqual(boolean,True)
 
-        #test the coordinates:
-        coordinates_list = []
-        for file in list:
-            file_object = open("output/text/"+file, "r")
-            text = file_object.read()
-            coordinates_list.extend(text.split(','))
+    #     #test the coordinates:
+    #     coordinates_list = []
+    #     for file in list:
+    #         file_object = open("output/text/"+file, "r")
+    #         text = file_object.read()
+    #         coordinates_list.extend(text.split(','))
         
-        self.assertEqual(len(coordinates_list),number)
-        shutil.rmtree("output/text/")
-        shutil.rmtree("output/")
+    #     self.assertEqual(len(coordinates_list),number)
+    #     shutil.rmtree("output/text/")
+    #     shutil.rmtree("output/")
 
     
         
