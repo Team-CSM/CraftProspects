30dba90d10f62f087d23b7bfaabd3105c505bf52
Dan <2193860c@student.gla.ac.uk>
2018-03-15 15:29:23 +0000

    Added new names for the folders.
    Former-commit-id: cfd256b9532903cd81d574ca80903cbba8f397d6 [formerly ce546c96340c158f3f9bb55ff82162a6b91c0eec]
    Former-commit-id: 7d43664cffe0f0f9eae1b51253ed67da7f0852d6

--- CI/test_launcherMac.py
+++ CI/test_launcherMac.py
@@ -15,36 +15,34 @@ class test_launcherMac(unittest.TestCase):
 
     # tests if number of slices created is equal to the number it was asked to be sliced.
     def testSliceNumber(self):
-        if not os.path.exists("output/"):
-            os.makedirs("output/")
+        if not os.path.exists("output1/"):
+            os.makedirs("output1/")
         imagepath = "CI/orig.jpg"
         number = 20
-        slice(number,imagepath,"output/")
-        list = os.listdir("output/")
-        print(list)
+        slice(number,imagepath,"output1/")
+        list = os.listdir("output1/")
         number_files = len(list)
-        shutil.rmtree("output/")
+        shutil.rmtree("output1/")
         self.assertEqual(number,number_files)
     
     def testPredict_text(self):
-  
-   
-        if not os.path.exists("output/"):
-            os.makedirs("output/")
+ 
+        if not os.path.exists("output2/"):
+            os.makedirs("output2/")
 
         imagepath = "CI/orig.jpg"
         number = 20 
-        slice(number,imagepath,"output/")
-        list = os.listdir("output/")
+        slice(number,imagepath,"output2/")
+        list = os.listdir("output2/")
         
-        if not os.path.exists("output/text/"):
-            os.makedirs("output/text/")
+        if not os.path.exists("output2/text/"):
+            os.makedirs("output2/text/")
         
         for var in range(len(list)):
-            list[var] = "output/"+list[var]
+            list[var] = "output2/"+list[var]
         
-        predict(list,class_to_name,"output/text/",load_model("CI/model.h5"))
-        list = os.listdir("output/text/")
+        predict(list,class_to_name,"output2/text/",load_model("CI/model.h5"))
+        list = os.listdir("output/2text/")
         
         boolean = False
         for file in list:
@@ -54,18 +52,18 @@ class test_launcherMac(unittest.TestCase):
                 boolean = True
             else:
                 boolean = False
-        self.assertEqual(True,boolean)
+        self.assertEqual(boolean,True)
 
         #test the coordinates:
         coordinates_list = []
         for file in list:
-            file_object = open("output/text/"+file, "r")
+            file_object = open("output2/text/"+file, "r")
             text = file_object.read()
             coordinates_list.extend(text.split(','))
-        shutil.rmtree("output/")
-
+        
         self.assertEqual(len(coordinates_list),number)
-
+        shutil.rmtree("output2/text/")
+        shutil.rmtree("output2/")
 
     
         
