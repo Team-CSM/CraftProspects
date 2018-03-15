7ff33ddc9d3e4c2390ad48813a933e54086d1fa3
Dan <2193860c@student.gla.ac.uk>
2018-03-15 15:23:53 +0000

    Update test_launcherMac.py
    Former-commit-id: c542a2cb53778c936ff90ddee32db8757c782d2f [formerly c429dba83ecf68df88c26f67005416e5580b8708]
    Former-commit-id: 0e265665d928aae0b8051b6beff3e87e2438c517

--- CI/test_launcherMac.py
+++ CI/test_launcherMac.py
@@ -27,37 +27,8 @@ class test_launcherMac(unittest.TestCase):
         self.assertEqual(number,number_files)
     
     def testPredict_text(self):
-        
-        # model = Sequential()
-        # model.add(Conv2D(32, (3, 3), input_shape=(150,150,3)))
-        # model.add(Activation('relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # model.add(Conv2D(32, (3, 3)))
-        # model.add(Activation('relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # model.add(Conv2D(64, (3, 3)))
-        # model.add(Activation('relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-        # model.add(Flatten())
-        # model.add(Dense(64))
-        # model.add(Activation('relu'))
-        # model.add(Dropout(0.5))
-        # model.add(Dense(1))
-        # model.add(Activation('sigmoid'))
-        # model.save("test.h5")
-            # Loads the pretrained 19-layer network.
-        # base_model = VGG19(include_top=False,
-        #                 weights='imagenet',
-        #                 input_shape=(64, 64, 3))
-        # model = Sequential()
-        # model.add(base_model)        
-        # model.add(Flatten())
-        # model.add(Dense(4, activation='softmax'))
-        # model.save("test.h5")
-        # #check if the text files correspond to class_to_name.
-        # #recreates the slices.
+  
+   
         if not os.path.exists("output/"):
             os.makedirs("output/")
 
@@ -91,10 +62,10 @@ class test_launcherMac(unittest.TestCase):
             file_object = open("output/text/"+file, "r")
             text = file_object.read()
             coordinates_list.extend(text.split(','))
-        
-        self.assertEqual(len(coordinates_list),number)
-        shutil.rmtree("output/text/")
         shutil.rmtree("output/")
+
+        self.assertEqual(len(coordinates_list),number)
+
 
     
         
