a1824818679fbe7f75187cc2d2a2cf3861951d80
Craig Massie <2207400m@student.gla.ac.uk>
2018-03-16 18:17:02 +0000

    Update categoriseData.py
    Former-commit-id: 2ef56d00ec5156e4bb9efd2700c1d0f978642fc5 [formerly d582736de6f91bf4a97d4dadb1b24a40355dfe04]
    Former-commit-id: 26ed0b3ecf91a3c63251b2a22fcf5ada843d49e0

--- Standalone/dataparse/categoriseData.py
+++ Standalone/dataparse/categoriseData.py
@@ -9,7 +9,6 @@ import os
 from shutil import copyfile
 
 
-
 def populateDict(csvDir, classes):
     """Inputs the directory of the CSV file and the name of all the classes. Appends item uniquely to a one category. 
 	Returns a dictionary of all the classes as keys with the images as the values."""
@@ -17,25 +16,25 @@ def populateDict(csvDir, classes):
 	alreadyAdded = []
 
 	for name in classes:
-		classFiles[name]=[]
-		csvFile=open(csvDir)
-		reader=csv.reader(csvFile)
+		classFiles[name] = []
+		csvFile = open(csvDir)
+		reader = csv.reader(csvFile)
 
 	for row in reader:
-	
+
 		if row[1].find('slash_burn') != -1 and row[0] not in alreadyAdded:
 			classFiles['slash_burn'].append(row[0])
 			alreadyAdded.append(row[0])
-	
+
 		elif row[1].find('artisinal_mine') != -1 and row[0] not in alreadyAdded:
 			classFiles['artisinal_mine'].append(row[0])
 			alreadyAdded.append(row[0])
-	
+
 		elif row[1].find('cloudy') != -1 and row[0] not in alreadyAdded:
 			if row[1].find('partly_cloudy') == -1:
 				classFiles['cloudy'].append(row[0])
 				alreadyAdded.append(row[0])
-	
+
 		elif row[1].find('clear primary') != -1 and row[0] not in alreadyAdded:
 			classFiles['clear primary'].append(row[0])
 			alreadyAdded.append(row[0])
@@ -43,37 +42,40 @@ def populateDict(csvDir, classes):
 	return classFiles
 
 
-def makeFolders(classes_list,path):
+def makeFolders(classes_list, path):
     """Inputs list classes, and string path. Creates the folders for all classes at the given path. Returns nothing."""
     for var in classes_list:
         if " " in var:
-            newpath=path+"/"+var.replace(" ","_")
+            newpath = path + "/" + var.replace(" ", "_")
         else:
-            newpath=path+"/"+var
+            newpath = path + "/" + var
         if not os.path.exists(newpath):
             os.makedirs(newpath)
 
-def relocateFiles(dictionary,path):
+
+def relocateFiles(dictionary, path):
     """Inputs the dictionary containing all the classes and their corresponding images, and the path for output. Creates folders for 
 	all the classes at the path, and relocates all images for the class to that folder. Returns nothing."""
     #gets the keys of the dictionary
-    keys=dictionary.keys()
+    keys = dictionary.keys()
 
     for key in keys:
         if " " in key:
-            newpath=path+"/"+key.replace(" ","_") #if there is a space in the name its replaced with an underscore.
+            # if there is a space in the name its replaced with an underscore.
+            newpath = path + "/" + key.replace(" ", "_")
         else:
-            newpath=path+"/"+key
+            newpath = path + "/" + key
 
         #gets the array of files:
         fileArray = dictionary.get(key)
 
         for file in fileArray:
-            oldfilePath = path+"/"+file+".jpg"
-            newFilePath = newpath +"/"+file+".jpg"
-            copyfile(oldfilePath,newFilePath)
+            oldfilePath = path + "/" + file + ".jpg"
+            newFilePath = newpath + "/" + file + ".jpg"
+            copyfile(oldfilePath, newFilePath)
 
 #--------------------------------------------------
+
 
 if __name__ == '__main__':
 	classes = ["clear primary", "cloudy", "artisinal_mine", "slash_burn"]
@@ -86,10 +88,5 @@ if __name__ == '__main__':
 		shuffle(classFiles[cl])
 		classFiles[cl] = classFiles[cl][:340]
 
-	makeFolders(classes,"train-jpg")
-	relocateFiles(classFiles,"train-jpg")
-
-
-
-
-
+	makeFolders(classes, "train-jpg")
+	relocateFiles(classFiles, "train-jpg")
